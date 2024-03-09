c4166cd9cf434c9900b7e70c7f904233bc3e9316
Nathan Brodin <nathan@brodin.dev>
2024-03-09 10:40:22 +0100

    feat: configured Portfolio branding

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: "Portfolio | Nathan Brodin",
+  url: "https://brodin.dev",
+  ogImage: "https://brodin.dev/og.png",
+  description:
+    "Nathan Brodin's portfolio. A software engineer with a passion for web development, design, and user experience.",
+  links: {
+    github: "https://github.com/NathanBrodin/Portfolio",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
