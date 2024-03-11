0eae0dc516db9570f8fc18b6445b84ead7bd8c36
Nathan Brodin <nathan@brodin.dev>
2024-03-11 17:42:07 +0100

    fix: bento grid

--- app/_components/corrected-bento.tsx
+++ app/_components/corrected-bento.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/utils";
-import { LucideIcon, TriangleAlert } from "lucide-react";
+import { ArrowRight, LucideIcon, TriangleAlert } from "lucide-react";
+import Link from "next/link";
 import { ReactNode } from "react";
 
 export function CorrectedBento() {
@@ -12,7 +13,9 @@ export function CorrectedBento() {
           title="Bento Card"
           description="This is a Bento Card with a warning icon"
           icon={TriangleAlert}
-          widget={<div></div>}
+          widget={
+            <div className="rdp p-3 absolute right-0 top-10 origin-top rounded-md border transition-all duration-300 ease-out [mask-image:linear-gradient(to_top,transparent_40%,#000_100%)] group-hover:scale-105"></div>
+          }
           size="sm"
         />
         <CorrectedBentoCard
@@ -59,7 +62,7 @@ function CorrectedBentoCard({
   return (
     <div
       className={cn(
-        "rounded-xl transform-gpu bg-transparent backdrop-blur-md min-h-[22rem] lg:min-h-min",
+        "group relative flex flex-col justify-between rounded-xl transform-gpu bg-transparent backdrop-blur-md min-h-[22rem] lg:min-h-min overflow-hidden",
         size === "sm" ? "lg:col-span-1" : "lg:col-span-2",
       )}
       style={{
@@ -67,11 +70,19 @@ function CorrectedBentoCard({
         boxShadow: "0 -20px 80px -20px #ffffff1f inset",
       }}
     >
+      <div>{widget}</div>
       <div className="pointer-events-none z-10 flex transform-gpu flex-col gap-1 p-6 transition-all duration-300 group-hover:-translate-y-10">
         <Icon className="h-12 w-12 origin-left transform-gpu text-neutral-700 transition-all duration-300 ease-in-out group-hover:scale-75" />
         <h3 className="text-xl font-semibold text-neutral-300">{title}</h3>
         <p className="max-w-lg text-neutral-400">{description}</p>
       </div>
+      <div className="pointer-events-none absolute bottom-0 flex w-full translate-y-10 transform-gpu flex-row items-center p-4 opacity-0 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100">
+        <div className="inline-flex items-center justify-center font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-8 rounded-md px-3 text-xs pointer-events-auto">
+          Learn more
+          <ArrowRight className="ml-2 h-4 w-4" />
+        </div>
+      </div>
+      <div className="pointer-events-none absolute inset-0 transform-gpu transition-all duration-300 group-hover:bg-neutral-800/10"></div>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,10 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-full min-h-screen">
       <Hero />
-      <CorrectedBento />
+      <div className="bg-gradient-to-bl from-black via-zinc-400/20 to-black">
+        <CorrectedBento />
+      </div>
+
       <About />
     </div>
   );
