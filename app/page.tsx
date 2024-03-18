f73901296f2e392e5e1ecadbdcc72d92c0cf80de
Nathan Brodin <nathan@brodin.dev>
2024-03-18 14:23:31 +0100

    perf: improved folder structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { DashedSeparator } from "@/components/dashed-separator";
 import About from "./_components/about";
 import Hero from "./_components/hero";
 
@@ -5,7 +6,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-full min-h-screen">
       <Hero />
-      <div className="w-full h-4 border-zinc-500/50 border-dashed border-b-2" />
+      <DashedSeparator />
       <About />
     </div>
   );
---
+++ components/dashed-separator.tsx
@@ -0,0 +1,5 @@
+export function DashedSeparator() {
+  return (
+    <div className="w-full h-4 border-zinc-500/50 border-dashed border-b-2" />
+  );
+}
