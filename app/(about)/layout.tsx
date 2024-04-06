dbb0b38abe5e1a046a9e94ca7c8653606a6726c3
Nathan Brodin <nathan@brodin.dev>
2024-04-06 16:10:31 +0200

    feat: improved mdx style

--- app/(about)/layout.tsx
+++ app/(about)/layout.tsx
@@ -1,7 +1,14 @@
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="bg-gradient-to-tl from-zinc-900 via-zinc-400/10 to-zinc-900 min-h-screen w-full flex flex-col justify-center">
+    <div className="bg-gradient-to-tl from-zinc-900 via-zinc-400/20 to-zinc-900 min-h-screen w-full flex flex-col justify-center">
       <div className="max-w-5xl px-6 py-20 mx-auto lg:px-8  md:py-24 lg:py-32">
+        <Link href="/#about">
+          <ArrowLeft className="w-6 h-6 text-zinc-300 mb-4 hover:text-zinc-100" />
+        </Link>
+
         {children}
       </div>
     </div>
--- mdx-components.tsx
+++ mdx-components.tsx
@@ -15,7 +15,7 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     h2: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
       <h2
         className={cn(
-          "font-heading mt-12 scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight first:mt-0",
+          "font-heading mt-12 scroll-m-20 border-b border-zinc-700 pb-2 text-2xl font-semibold tracking-tight first:mt-0",
           className,
         )}
         {...props}
@@ -105,7 +105,7 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
       className,
       ...props
     }: React.HTMLAttributes<HTMLTableElement>) => (
-      <div className="my-6 w-full overflow-y-auto">
+      <div className="my-6 w-full overflow-y-auto bg-zinc-900">
         <table className={cn("w-full", className)} {...props} />
       </div>
     ),
@@ -113,10 +113,7 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
       className,
       ...props
     }: React.HTMLAttributes<HTMLTableRowElement>) => (
-      <tr
-        className={cn("m-0 border-t p-0 even:bg-muted", className)}
-        {...props}
-      />
+      <tr className={cn("m-0 border-t even:bg-muted", className)} {...props} />
     ),
     th: ({
       className,
@@ -124,7 +121,7 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     }: React.HTMLAttributes<HTMLTableCellElement>) => (
       <th
         className={cn(
-          "border px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right",
+          "border border-zinc-700 px-4 py-2 text-left font-bold bg-zinc-800 [&[align=center]]:text-center [&[align=right]]:text-right",
           className,
         )}
         {...props}
@@ -136,7 +133,7 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     }: React.HTMLAttributes<HTMLTableCellElement>) => (
       <td
         className={cn(
-          "border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right",
+          "border border-zinc-700 px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right",
           className,
         )}
         {...props}
