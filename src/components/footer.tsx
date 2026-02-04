308283df5378fab8ca8e001a8786e9760954c196
Nathan Brodin <nathan@brodin.dev>
2026-02-04 15:49:20 +0100

    feat: footer and page structure

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,44 +1,22 @@
 import { Diamond } from './ui/diamond'
 import { Icons } from './ui/icons'
+import { Separator } from './ui/separator'
 
 export function Footer() {
   return (
     <footer className="px-4 border-t flex item-center justify-center ">
-      <div className="flex item-center justify-between border-x w-full px-4 py-1 max-w-5xl relative">
-        <div className="flex gap-1 items-center">
-          <p className="text-muted-foreground text-sm">
-            Built by{' '}
-            <a
-              className="link"
-              href="https://brodin.dev"
-              target="_blank"
-              rel="noopener"
-            >
-              Nathan
-            </a>
-          </p>
-          <span>·</span>
-          <p className="text-muted-foreground text-sm">
-            Inspired by{' '}
-            <a
-              className="link"
-              href="https://chanhdai.com/"
-              target="_blank"
-              rel="noopener"
-            >
-              Chanhdai
-            </a>{' '}
-            and{' '}
-            <a
-              className="link"
-              href="https://zed.dev/"
-              target="_blank"
-              rel="noopener"
-            >
-              Zed
-            </a>
-          </p>
-        </div>
+      <div className="flex item-center justify-between border-x w-full px-4  max-w-5xl relative">
+        <p className="text-muted-foreground text-sm py-2">
+          Built by{' '}
+          <a
+            className="link"
+            href="https://brodin.dev"
+            target="_blank"
+            rel="noopener"
+          >
+            Nathan
+          </a>
+        </p>
         <div className="mx-auto flex items-center justify-center gap-3 border-x border-edge bg-background px-4">
           <a
             className="flex items-center text-muted-foreground transition-colors hover:text-foreground"
@@ -49,6 +27,7 @@ export function Footer() {
             <Icons.x className="size-4" />
             <span className="sr-only">X</span>
           </a>
+          <Separator orientation="vertical" />
           <a
             className="flex items-center text-muted-foreground transition-colors hover:text-foreground"
             href="https://github.com/NathanBrodin?utm_source=brodin-dev"
@@ -58,6 +37,7 @@ export function Footer() {
             <Icons.github className="size-4" />
             <span className="sr-only">GitHub</span>
           </a>
+          <Separator orientation="vertical" />
           <a
             className="flex items-center text-muted-foreground transition-colors hover:text-foreground"
             href="https://www.linkedin.com/in/nathan-brodin?utm_source=brodin.dev"
@@ -68,6 +48,26 @@ export function Footer() {
             <span className="sr-only">LinkedIn</span>
           </a>
         </div>
+        <p className="text-muted-foreground text-sm py-2">
+          Inspired by{' '}
+          <a
+            className="link"
+            href="https://chanhdai.com/"
+            target="_blank"
+            rel="noopener"
+          >
+            Chanhdai
+          </a>{' '}
+          and{' '}
+          <a
+            className="link"
+            href="https://zed.dev/"
+            target="_blank"
+            rel="noopener"
+          >
+            Zed
+          </a>
+        </p>
         <Diamond top left />
         <Diamond top right />
       </div>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,3 @@
-import { Diamond } from '@/components/ui/diamond'
 import { Section } from '@/components/ui/section'
 import { SectionDivider } from '@/components/ui/section-divider'
 import { createFileRoute } from '@tanstack/react-router'
@@ -7,7 +6,7 @@ export const Route = createFileRoute('/')({ component: App })
 
 function App() {
   return (
-    <main className="px-4 flex flex-col items-center ">
+    <main className="px-4 flex flex-col items-center flex-1 h-full">
       <Section className="py-12">
         <div className="flex flex-col">
           <h1 className="font-display text-balance scroll-mt-24 text-primary font-normal text-center ">
@@ -19,7 +18,9 @@ function App() {
         </div>
       </Section>
       <SectionDivider />
-      <Section className="h-[300px]">Map of the world should go here</Section>
+      <Section className="h-full flex-1">
+        Map of the world should go here
+      </Section>
     </main>
   )
 }
